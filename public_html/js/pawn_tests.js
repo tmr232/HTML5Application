ed1157f8b3df8204e3f6fabaa4e6da5c76bb42ca
bahar <bahar@192.168.0.100>
2012-10-26 14:44:07 +0200

    Started writing pawn and map code. Lots of js learning for me...

---
+++ public_html/js/pawn_tests.js
@@ -0,0 +1,109 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Utility Functions
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function createArray2D(x, y, initCallback)
+{
+    var array = new Array(x);
+    for (var i = 0; i < x; ++i)
+    {
+        array[i] = new Array(y);
+        for (var j = 0; j < y; ++j)
+        {
+            array[i][j] = initCallback(i, j, x, y);
+        }
+    }
+
+    return array;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Classes
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function ExceptionOutOfRange(message)
+{
+    this.name = "ExceptionOutOfRange";
+    this.message = (message || "");
+};
+ExceptionOutOfRange.prototype = Error.prototype;
+
+function Pawn() {}
+
+function Cell(content)
+{
+    this.content = null;
+    if (arguments.length === 1)
+    {
+        this.content = content;
+    }
+
+    this.getContent = function()
+    {
+        return this.content;
+    };
+
+    this.setContent = function(content)
+    {
+        var temp = this.content;
+        this.content = content;
+        return temp;
+    };
+
+    this.isEmpty = function()
+    {
+        return this.content === null;
+    };
+
+    this.empty = function()
+    {
+        var temp = this.content;
+        this.content = null;
+        return temp;
+    };
+
+    this.toString = function()
+    {
+        return this.content.toString();
+    };
+}
+Cell.prototype.toString = Cell.toString();
+
+function Map() {
+    this.map = null;
+    this.width = null;
+    this.height = null;
+
+    this.initMap = function(width, height) {
+        this.map = createArray2D(width, height, function() {return new Cell();});
+        this.width = width;
+        this.height = height;
+    };
+    this.updateMap = function() {};
+    this.getCell = function(x, y) {
+        if ((x < 0) || (x >= this.width) || (y < 0) || (y >= this.height))
+        {
+            throw new ExceptionOutOfRange("Cell location out of map.");
+        }
+        else
+        {
+            return this.map[x][y];
+        }
+    };
+}
+
+//          updateMap:
+//              for each pawn:
+//                  updatePawn:
+//                      find target
+//                      check if route empty
+//                      move / attack / dont move
+//
+
+// Testing code:
+function test_pawn()
+{
+    var map = new Map();
+    map.initMap(3,3);
+    console.log(map.getCell(2,2));
+    map.getCell(4,4);
+}
